6220fccc88caea15267fb2fee70195af4c67cffc
bilal bedine <bilal.bedine@devinci.fr>
2020-10-01 12:50:08 +0200

    commit finsh algo

--- algo.py
+++ algo.py
@@ -7,6 +7,7 @@ class FibonacciHeap:
         while i <= len(self.trees):
             self.trees.append([value])
             break
+        print("insert success")
         return self.trees
 
 
@@ -15,6 +16,7 @@ class FibonacciHeap:
         for val in self.trees:
             array_min.append(val)
         a = sorted(array_min)
+        print("le minimum est trouver")
         return a[0]
 
 
@@ -22,25 +24,24 @@ class FibonacciHeap:
         x = self.find_min()
         for v in self.trees:
             if x == v:
-                print(v)
                 min = v
                 self.trees.remove(v)
                 print("le minimum est suprimer")
+                # print(min[0])
                 return min[0]
 
     def merge(self):
         delete = self.delete_min()
-        print(delete)
-        print(self.trees)
+        # print(delete)
+        # print(self.trees)
         for o in self.trees:
             x = len(o)
-            print(x)
             for v in self.trees:
                 y = len(v)
                 if x == y:
                     self.trees[0].append(delete)
-        print(self.trees)
-        return self.trees
+            print("merge success")
+            return None
 
 
 
